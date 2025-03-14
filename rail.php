c74eb80676ad6bc3ea77887c1343c5183812a27e
Fabiano Marques <88325341+Fabiano-Marques@users.noreply.github.com>
2025-03-14 18:51:40 -0300

    usando railway

---
+++ rail.php
@@ -0,0 +1,18 @@
+<?php
+$url = "detectarfaces-production.up.railway.app"; // Substitua pela URL real
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Desative se houver problemas com SSL
+
+$response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+if ($http_code == 200) {
+    echo "Resposta recebida: " . $response;
+} else {
+    echo "Falha ao acessar a URL. Código HTTP: " . $http_code;
+}
+?>
